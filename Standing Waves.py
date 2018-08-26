86131fd32eedaa0b798a962095b2b8d173d612f5
zoecq001 <42721682+zoecq001@users.noreply.github.com>
2018-08-26 16:31:51 -0400

    Update Standing Waves.py

--- Standing Waves.py
+++ Standing Waves.py
@@ -1,36 +1,21 @@
 
 # coding: utf-8
 
-# In[1]:
-
 from math import *
 import scipy as sp
 import matplotlib.pyplot as plt
 get_ipython().magic('matplotlib notebook')
 
-
-# In[2]:
-
 x=sp.linspace(-2,2,201)
 
 print(x)
-# In[3]:
 
 y=x**3
 
-
-# In[4]:
-
 print(y)
-
-
-# In[5]:
 
 def f1(x):
     return x**3
-
-
-# In[6]:
 
 plt.figure()
 plt.plot(x,y)
@@ -40,9 +25,6 @@ plt.title('this is the title')
 plt.axhline(0)
 plt.grid(True)
 plt.ylim(-12,12)
-
-
-# In[33]:
 
 x=sp.linspace(0,1,201)
 v=1
@@ -58,8 +40,6 @@ def f2(x):
     return A*sp.sin(sp.pi*2*x/lamda)*cos(omega*t)
 
 
-# In[36]:
-
 plt.figure()
 plt.plot(x,f2(x))
 plt.xlabel('x')
@@ -68,6 +48,3 @@ plt.title('standing waves')
 plt.axhline(0)
 plt.grid(True)
 plt.ylim(-2,2)
-
-
-
